df2165d7bee9921d08b1aa778fcf853984ab6def
chengongwei <1391581728@qq.com>
2021-11-05 14:42:18 +0800

    fix

--- src/pages/Dom/flushSync.tsx
+++ src/pages/Dom/flushSync.tsx
@@ -1,31 +1,35 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Index = () => {
 
     const [state, setState] = useState(0)
 
-
     const handerClick = () => {
-        setTimeout(()=>{
-            setState( 1  )
+        
+        setTimeout(() => {
+            setState(1)
         })
-        setState(2  )
-        ReactDOM.flushSync(()=>{
+
+        setState(2)
+
+        ReactDOM.flushSync(() => {
             setState(3)
         })
-        setState( 4  )
+
+        setState(4)
     }
 
+    // 首先打印 0
+    // 点击按钮后依次打印 3 4 1
+    console.log(state)
 
-    
-        // 首先打印 0
-        // 点击按钮后依次打印 3 4 1
-        console.log(state)
-        return <div>
-            <div>{ state }</div>
+    return (
+        <div>
+            <div>{state}</div>
             <button onClick={handerClick} >测试flushSync</button>
         </div>
+    )
 
 }
 
